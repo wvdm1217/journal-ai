b9442bf342dbf72f5b916aa69eac25dfdd74c22e
Werner van der Merwe <werner@spatialedge.ai>
2025-05-02 16:27:54 +0000

    chore: add mypy for type checking

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -6,13 +6,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # Install UV Dependencies
 uv venv
 source .venv/bin/activate
-uv sync --locked --all-extras --dev
-
-# Install ruff
-uv tool install ruff
-
-# Install pre-commit
-uv tool install pre-commit
+uv sync --locked --dev --group=test
 
 # Install pre-commit hooks
 pre-commit install
